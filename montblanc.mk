80c32faca9a3067c35861781db95fdfa44c424a1
Alin Jerpelea <jerpelea@gmail.com>
2012-11-10 09:21:53 +0200

    montblanc: add iw blob and corect wifi setup
    Change-Id: Ifc54f91c90344d9a9a9e11f703919e43d4fe29a3

--- montblanc.mk
+++ montblanc.mk
@@ -2,6 +2,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/sony/montblanc-common/recovery/recovery.mk)
 
+$(call inherit-product-if-exists, vendor/sony/montblanc-common/montblanc-vendor-blobs.mk)
+
 DEVICE_PACKAGE_OVERLAYS += device/sony/montblanc-common/overlay
 
 # Permissions
