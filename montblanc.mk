83eca3b973861e81b6320fc04eeae1b67e715661
Alin Jerpelea <jerpelea@gmail.com>
2013-01-08 19:31:12 +0200

    montblanc: storage changes for JB
    Change-Id: I30952279b929ee49fd3022a42df95163fd245405

--- montblanc.mk
+++ montblanc.mk
@@ -68,6 +68,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 # Custom init / uevent
 PRODUCT_COPY_FILES += \
     device/sony/montblanc-common/config/init.rc:root/init.rc \
+    device/sony/montblanc-common/config/fstab.st-ericsson:root/fstab.st-ericsson \
     device/sony/montblanc-common/config/init.st-ericsson.rc:root/init.st-ericsson.rc \
     device/sony/montblanc-common/config/ueventd.st-ericsson.rc:root/ueventd.st-ericsson.rc
 
