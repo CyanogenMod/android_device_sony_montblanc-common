c3ee92e3a1ac8e9884f7c1a1ca2e426659fc289c
Alin Jerpelea <jerpelea@gmail.com>
2013-03-21 18:25:24 +0200

    montblanc: increase background apps to 10
    Change-Id: I897d9319301c44f689d906b63df76a468d35c5b8

--- montblanc.mk
+++ montblanc.mk
@@ -97,7 +97,7 @@ PRODUCT_COPY_FILES += \
     device/sony/montblanc-common/config/cacert.txt:system/etc/suplcert/cacert.txt
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    sys.mem.max_hidden_apps=3
+    sys.mem.max_hidden_apps=10
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp \
